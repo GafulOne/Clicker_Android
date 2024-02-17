85adab132d49e1a75856253987b16aa8b7d8e7d9
KseniaV <kseniavolkova555@gmail.com>
2024-02-17 15:07:04 +0300

    asdd

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
     ImageButton plusButton;
-    Button minusButton;
-    Button dropButton;
     private long score = 0;
+    private int multiplier = 2;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,99 +27,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mainText = (TextView) findViewById(R.id.main_Text);
         plusButton = (android.widget.ImageButton) findViewById(R.id.plus_Button);
-        minusButton = (Button) findViewById(R.id.minus_Button);
-        dropButton = (Button) findViewById(R.id.drop_Button);
         plusButton.setOnClickListener(clickListener);
-        minusButton.setOnClickListener(clickListener);
-        dropButton.setOnClickListener(clickListener);
     }
 
-    View.OnClickListener clickListener = new View.OnClickListener()
+    public void onClick(View view)
     {
-        @Override
-        public void onClick(View v)
-        {
-            if(v.getId() == R.id.plus_Button)
-            {
-                score ++;
-                if (score == 12 || score == 13 || score == 14)
-                {
-                    String s = "Кнопка нажата " + score + " раз";
-                    mainText.setText(s.toCharArray(),0, s.length());
-                    plusButton.setOnClickListener(clickListener);
-                }
-                else
-                {
-                    if (score % 10 == 2 || score % 10 == 3 || score % 10 == 4)
-                    {
-                        String s = "Кнопка нажата " + score + " раза";
-                        mainText.setText(s.toCharArray(),0, s.length());
-                        plusButton.setOnClickListener(clickListener);
-                    }
-                    else
-                    {
-                        String s = "Кнопка нажата " + score + " раз";
-                        mainText.setText(s.toCharArray(),0, s.length());
-                        plusButton.setOnClickListener(clickListener);
-                    }
-                }
-            }
+        Intent intent = new Intent(this, Shop.class);
+        startActivity(intent);
+    }
+   View.OnClickListener clickListener = new View.OnClickListener()
+   {
+       @Override
+       public void onClick(View v)
+       {
+           if(v.getId() == R.id.plus_Button) {
+               score++;
+               String s = "" + score + "";
+               mainText.setText(s.toCharArray(), 0, s.length());
+               plusButton.setOnClickListener(clickListener);
+           }
 
-            if(v.getId() == R.id.minus_Button)
-            {
-                score --;
-                if (score == -12 || score == -13 || score == -14)
-                {
-                    String s = "Кнопка нажата " + score + " раз";
-                    mainText.setText(s.toCharArray(),0, s.length());
-                    minusButton.setOnClickListener(clickListener);
-                }
-                else
-                {
-                    if (score % 10 == -2 || score % 10 == -3 || score % 10 == -4)
-                    {
-                        String s = "Кнопка нажата " + score + " раза";
-                        mainText.setText(s.toCharArray(),0, s.length());
-                        minusButton.setOnClickListener(clickListener);
-                    }
-                    else
-                    {
-                        String s = "Кнопка нажата " + score + " раз";
-                        mainText.setText(s.toCharArray(),0, s.length());
-                        minusButton.setOnClickListener(clickListener);
-                    }
-                }
-            }
+           if (score == 10)
+           {
+               Toast.makeText(getApplicationContext(), "Достижение получено! Покемон эволюционировал!",Toast.LENGTH_LONG).show();
+               plusButton.setImageResource(R.drawable.charmeleon);
 
-            if(v.getId() == R.id.drop_Button)
-            {
-                score = 0;
-                String s = "Кнопка нажата " + score + " раз";
-                mainText.setText(s.toCharArray(),0, s.length());
-                dropButton.setOnClickListener(clickListener);
-            }
-            if (score == 50)
-            {
-                Toast.makeText(getApplicationContext(), "Достижение получено! Покемон эволюционировал!",Toast.LENGTH_LONG).show();
-                plusButton.setImageResource(R.drawable.charmeleon);
-            }
-            if (score == 49)
-            {
-                plusButton.setImageResource(R.drawable.charmander);
-            }
-            if (score == 100)
-            {
-                Toast.makeText(getApplicationContext(), "Достижение получено! Покемон эволюционировал!",Toast.LENGTH_LONG).show();
-                plusButton.setImageResource(R.drawable.charizard);
-            }
-            if (score == 99)
-            {
-                plusButton.setImageResource(R.drawable.charmeleon);
-            }
-            if (score == 1000)
-            {
-                Toast.makeText(getApplicationContext(), "Достижение получено: Потрогай траву",Toast.LENGTH_LONG).show();
-            }
-        }
-    };
+           }
+           if (score == 100)
+           {
+               Toast.makeText(getApplicationContext(), "Достижение получено! Покемон эволюционировал!",Toast.LENGTH_LONG).show();
+               plusButton.setImageResource(R.drawable.charizard);
+           }
+           if (score == 99)
+           {
+               plusButton.setImageResource(R.drawable.charmeleon);
+           }
+           if (score == 1000)
+           {
+               Toast.makeText(getApplicationContext(), "Достижение получено: Потрогай траву",Toast.LENGTH_LONG).show();
+           }
+       }
+   };
 }
